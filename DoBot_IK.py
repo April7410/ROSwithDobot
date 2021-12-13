feee31d2f80059ff682a11c89a310e45ce85a395
Varun-ABC <89807847+Varun-ABC@users.noreply.github.com>
2021-12-13 17:55:46 -0500

    Add files via upload

--- DoBot_IK.py
+++ DoBot_IK.py
@@ -2,7 +2,7 @@ import numpy as np
 from numpy import transpose, cross, arctan2, arcsin
 from numpy.linalg import norm
 from subprobs import *
-###############dummy
+
 def DoBot_IK(p0T):
     l1 = 103; l2 = 135; l3 = 160; l4 = 50; l5 = 75;
     ex = transpose([1,0,0]); ey = transpose([0, 1,0]); ez = transpose([0,0,1]);
@@ -15,24 +15,16 @@ def DoBot_IK(p0T):
     q2sol =  np.zeros((2,2))
 
     for i in [0,1]:
-        print("rot(h1, -q1sol[i])")
-        print(rot(h1, -q1sol[i]))
-        print("(p0T - p01) - p4T")
-        print((p0T - p01) - p4T)
         d = norm(rot(h1, -q1sol[i]) * (p0T - p01) - p4T)
 
         q3sol = np.append(q3sol, subprob3(ey, -p34, p23, d), axis=1)
         q3sol = np.delete(q3sol, 0,1)
-        print("*****q3sol***")
-        print(q3sol)
         for j in [0,1]:
             q2sol[i,j] = subprob1(ey, p23+ rot(h3, q3sol[j,i]) @ p34, rot(h1, - q1sol[i]) @ (p0T - p01) - p4T)
 
     qsol = np.array([[float(q1sol[0]),float(q1sol[0]), float(q1sol[1]), float(q1sol[1])],
                      [q2sol[0,0], q2sol[1,0], q2sol[0,1], q2sol[1,1]],
                      [q3sol[0,0], q3sol[1,0], q3sol[0,1], q3sol[1,1]]])
-    print("*******qsol**********")
-    print(qsol)
-    qsol = np.append(qsol, -(qsol[2,:] + qsol[1,:]), 1)
-    print(qsol)
+
+    qsol = np.append(qsol, [-(qsol[2,:] + qsol[1,:])], 0)
     return qsol
