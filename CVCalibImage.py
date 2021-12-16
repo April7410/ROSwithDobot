ffc748d5e7c856ad956ece10d096220d5c463996
April7410 <39639833+April7410@users.noreply.github.com>
2021-12-16 17:47:54 -0500

    Upload Files

--- CVCalibImage.py
+++ CVCalibImage.py
@@ -5,25 +5,25 @@ import glob
 #import os
 
 # Define the dimensions of checkerboard
-CHECKERBOARD = (5, 6)
-FrameSize = (640, 480)
+CHECKERBOARD = (5, 6)   # subject to change
+FrameSize = (640, 480)  # or later use the "grayColor.shape[::-1]" instead
 
-# stop the iteration when specified
-# accuracy, epsilon, is reached or
-# specified number of iterations are completed.
+# stop the iteration when specified accuracy, epsilon, is reached 
+# or specified number of iterations are completed.
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-obj3dpt = []    # Vector for 3D points
-flat2dpt = []   # Vector for 2D points
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane
 
 # 3D points real world coordinates
 objectp3d = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
 objectp3d[0, :, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 prev_img_shape = None
 
-images = glob.glob('*.png') # Takes all current directory png files alone
-#images = list('chess5x6.png') # Takes all current directory png files alone
+# prepare all the .png to calibrate before executing this line
+images = glob.glob('*.png') # Takes all current directory png files
 ReturnList = []
+
 for pic in images:
 	print(pic)
 	image = cv2.imread(pic)
@@ -41,11 +41,11 @@ for pic in images:
 	# refine the pixel coordinates and display
 	# them on the images of checkerboard
 	if ret: 	# return of cv2.findChessboardCorners==True
-		obj3dpt.append(objectp3d)
+		objpoints.append(objectp3d)
 		# Refining pixel coordinates for given 2d points.
 		corners2 = cv2.cornerSubPix(grayColor, corners, (11, 11), (-1, -1), criteria)
 		#print(len(corners2))
-		flat2dpt.append(corners2)
+		imgpoints.append(corners2)
 
 		# Draw and display the corners
 		image = cv2.drawChessboardCorners(image, CHECKERBOARD, corners2, ret)
@@ -53,7 +53,7 @@ for pic in images:
 		cv2.waitKey(0)
 	else:
 		cv2.imshow('ChessNotFoundImage', image)
-		cv2.waitKey(10)
+		cv2.waitKey(500)
 
 cv2.destroyAllWindows()
 
@@ -64,10 +64,10 @@ h, w = image.shape[:2]
 # and its corresponding pixel coordinates of the
 # detected corners (twodpoints)
 retval, CamMatrix, distortion, r_vecs, t_vecs = cv2.calibrateCamera(
-	obj3dpt, flat2dpt, FrameSize, None, None)
-	#grayColor.shape[::-1], None, None)
+	objpoints, imgpoints, FrameSize, None, None)
+	#objpoints, imgpoints, grayColor.shape[::-1], None, None)
 #print(grayColor.shape[::-1])
-print("test what it lookslike\n", cv2.calibrateCamera(obj3dpt, flat2dpt, FrameSize, None, None))
+print("test what it lookslike\n", cv2.calibrateCamera(objpoints, imgpoints, FrameSize, None, None))
 
 # Displaying required output
 print(" Calibrated pic: ", ReturnList)
--- CVdetection_AprilTag.py
+++ CVdetection_AprilTag.py
@@ -4,18 +4,19 @@ import apriltag
 #import cvzone
 #from cvzone.HandTrackingModule import HandDetector
 # define a video capture object
-cap = cv2.VideoCapture("/dev/video0")
-#cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture("/dev/video0")
+# cap = cv2.VideoCapture(0)
 
-while(True):
-    success, image = cap.read()
-    #print(success)
-    cv2.imshow('frame', image)
-    #cv2.imshow('frame',cap.read()[1])
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# while(True):
+#     success, image = cap.read()
+#     print(success)
+#     # cv2.waitKey(0)
+#     cv2.imshow('frame', image)
+#     #cv2.imshow('frame',cap.read()[1])
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-img=cv2.imwrite('to_detectApril.png', image)
+# img=cv2.imwrite('to_detectApril.png', image)
 
 #img = cv2.imread(image.cv2.IMREAD_GRAYSCALE)
 detector = apriltag.Detector()
